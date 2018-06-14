ae69601716f0b8382001a29434e6edbc4d1e42f3
zjx <1633156921@qq.com>
2018-06-14 20:24:14 +0800

    登录界面搭建

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+// 导入elementUI
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// 全局样式
+import '@/assets/css/index.css';
 
-Vue.config.productionTip = false
+// 全局配置ElementUI
+Vue.use(ElementUI);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +20,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+// 导入组件
+import Login from '@/views/login';
+
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    
+    { name: '1', path: '/', redirect: { name: 'login' } },
+    { name: 'login', path: '/login', component: Login }
   ]
-})
+});
